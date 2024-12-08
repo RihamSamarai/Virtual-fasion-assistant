25f07c5aaf0ff178f2ee634d16708f47170a0b13
RihamSamarai <riham.samarai@gmail.com>
2024-12-08 18:49:19 +0100

    added clothing suggestions info

--- fasion_assistant/src/components/ClothingSuggestions.js
+++ fasion_assistant/src/components/ClothingSuggestions.js
@@ -1,16 +1,14 @@
 
 
-const ClothingSuggestion = ([suggestions]) => {
+const ClothingSuggestion = () => {
     return (
         <div className="p-4">
             <h3 className="text-lg font-semibold">
                 Recommended Outfits
             </h3>
-            <ul>{suggestions.map((item, index) => (
-          <li key={index} className="mb-2">
-            {item}
-          </li>
-        ))}</ul>
+            <p className="text-lg text-gray-600 mb-8">Discover the latest outfit recommendations for you.</p>
+
+        
         </div>
 
     );
