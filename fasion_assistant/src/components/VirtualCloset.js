841af6dde17e2b619f338aa89944bdbb462d00af
RihamSamarai <riham.samarai@gmail.com>
2024-12-08 18:47:39 +0100

    add closet page info

--- fasion_assistant/src/components/VirtualCloset.js
+++ fasion_assistant/src/components/VirtualCloset.js
@@ -4,12 +4,8 @@
 const VirtualCloset = () => {
     return(
         <div className="p-4">
-            <h3>Your Virtual Closet</h3>
-                <ul>
-                    {items.map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
+            <h3 className="text-4xl font-bold text-gray-800 mb-4">Your Virtual Closet</h3>
+            <p className="text-lg text-gray-600 mb-8">Manage and view your saved outfits here.</p>
             
         </div>
     );
