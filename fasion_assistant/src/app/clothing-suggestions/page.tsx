62ae82cfa69bdb62d2c2a547f95549b247f91453
RihamSamarai <riham.samarai@gmail.com>
2024-12-08 18:51:00 +0100

    added clothing suggestions comonents

--- fasion_assistant/src/app/clothing-suggestions/page.tsx
+++ fasion_assistant/src/app/clothing-suggestions/page.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import ClothingSuggestion from '@/components/ClothingSuggestions'
 import ExternalDataFetcher from '@/components/ExternalDataFetcher'
 
 export default function clothingSuggestionPage(){
@@ -8,9 +9,9 @@ export default function clothingSuggestionPage(){
     return (
         <div className="max-w-4xl mx-auto p-6 text-center">
           <Header />
+          <ClothingSuggestion />
           
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">Clothing Suggestions</h1>
-          <p className="text-lg text-gray-600 mb-8">Discover the latest outfit recommendations for you.</p>
+          
           
           <ExternalDataFetcher/>
           <Footer />
