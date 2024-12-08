7f9093532b213b899bd37eae0ba37ef86614d55a
RihamSamarai <riham.samarai@gmail.com>
2024-12-08 18:48:28 +0100

    add virtual closet componet

--- fasion_assistant/src/app/virtual-closet/page.tsx
+++ fasion_assistant/src/app/virtual-closet/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import VirtuaCloset from '@/components/VirtualCloset';
 
 
 
@@ -9,9 +10,9 @@ export default function virtualCloset(){
     return (
         <div className="max-w-4xl mx-auto p-6 text-center">
         
-     <Header/>
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">Your Virtual Closet</h1>
-          <p className="text-lg text-gray-600 mb-8">Manage and view your saved outfits here.</p>
+          <Header/>
+          <VirtuaCloset />
+          
           
           <Footer />
         </div>
